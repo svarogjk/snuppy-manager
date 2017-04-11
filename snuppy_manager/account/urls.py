217a80c26af1784b3af3bb023e3d4d5abad3e7bf
svarogjk <svarogjk1989@gmail.com>
2017-04-11 23:52:13 +0300

    trying to add toolbar

--- snuppy_manager/account/urls.py
+++ snuppy_manager/account/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
 from . import views
 
 from django.contrib.auth.views import login
@@ -14,5 +16,11 @@ urlpatterns = [
 
     url(r'^$', views.dashboard, name='dashboard'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
 
 
