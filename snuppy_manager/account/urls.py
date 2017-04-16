43f8b480bc29d6df5508c20dd2064716648aa440
svarogjk <svarogjk1989@gmail.com>
2017-04-16 21:22:00 +0300

    registration form

--- snuppy_manager/account/urls.py
+++ snuppy_manager/account/urls.py
@@ -13,19 +13,27 @@ from django.contrib.auth.views import (login, logout,
 
 urlpatterns = [
 
+
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    url(r'^register/$', views.register, name='register'),
+
     # login / logout urls
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
 
-    url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^(?P<user_id>[0-9]+)/$', views.show_all_app, name='show_all_app'),
+    #change password urls
     url(r'^password-change/$',
-        password_change,
-        name='password_change'),
+            password_change,
+            name='password_change'),
     url(r'^password-change/done/$',
         password_change_done,
         name='password_change_done'),
+
+    #show all apps
+    url(r'^(?P<user_id>[0-9]+)/$', views.show_all_app, name='show_all_app'),
+
 
     #restore password urls
     url(r'^password-reset/$',
@@ -43,12 +51,15 @@ urlpatterns = [
     url(r'^password-reset/complete/$',
         password_reset_complete,
         name='password_reset_complete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+]
+
+
 
 
--- snuppy_manager/account/views.py
+++ snuppy_manager/account/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.views.decorators.csrf import csrf_protect
 from .core.create_uid import create_uid
 
@@ -49,3 +49,24 @@ def show_all_app(request, user_id):
     _user = User.objects.get(pk=user_id)
     _app = Application.objects.filter(user=_user)
     return render(request, 'account/all_app.html', {'applications':_app})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            #Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Save the User object
+            new_user.save()
+            return render(request,
+                          'registration/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'registration/register.html',
+                  {'user_form': user_form})
