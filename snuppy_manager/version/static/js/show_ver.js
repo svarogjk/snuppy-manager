61332db7e6b33fca8a8ceb195007676abb74dffa
Aleksandr Rebrik <ackud@mail.ru>
2017-07-02 22:37:43 +0300

    add ajax for adding version

---
+++ snuppy_manager/version/static/js/show_ver.js
@@ -0,0 +1,61 @@
+(function(){
+    'use strict';
+
+    function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+    }
+
+    function send_data(){
+
+        var ver_number = document.getElementById('ver_number').value;
+        var app_id = document.getElementById('app_id').value;
+        var os_type = document.getElementById('os_type').value;
+
+        var data = {
+            app_id : app_id,
+            os_type : os_type,
+            ver_number : ver_number
+        };
+
+        $.ajaxSetup({
+                headers: { "X-CSRFToken": getCookie("csrftoken") }
+            });
+
+        $.ajax({
+              url: "",
+              type: "post",
+              data: data,
+              datatype: 'json',
+              success: function(data){
+                    alert('Приложение добавленно!');
+                    location.reload();
+              },
+              error: function(){
+                  alert('Ошибка! Какая-нибудь...');
+              },
+        });
+    }
+
+    $("#add_version").submit(function(event) {
+        event.preventDefault();
+        $('#add_ver_modal').modal('show');
+    });
+
+    $("#add_ver_btn").submit(function(event) {
+        event.preventDefault();
+        send_data();
+    });
+
+
+}());
--- snuppy_manager/version/views.py
+++ snuppy_manager/version/views.py
@@ -1,40 +1,76 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.views.generic.list import View
+from django.utils.decorators import method_decorator
 
 from account.models import Profile
 from application.models import Application
 from .models import Version
 
-from .core import get_changes, ApiConnect
+from .core.ApiConnect import ApiConnect
+from .core.get_changes import get_changes
 
 
 #VERSIONS
+class ShowVersion(View):
 
-@login_required
-def show_version(request):
-    if request.method == 'GET':
-        # например
-        # account/version/?app_id=1&ver_type=W
+    @method_decorator(login_required)
+    def get(self, request):
         profile = Profile.objects.get(user=request.user)
         _app_id = request.GET.get('app_id')
         _app = Application.objects.get(id=_app_id)
 
         _ver_small = request.GET.get('ver_type')
-        _ver_type = Version.LOOKUP_CHOISE[_ver_small] # получаем полное имя ОС
+        _ver_type = Version.LOOKUP_CHOISE[_ver_small]  # получаем полное имя ОС
 
         _versions = Version.objects.filter(application=_app, ver_type=_ver_small)
 
         privilege = _app.group.rule_set.get(profile=profile).rule
 
         return render(request, 'version/show.html', {
-                                                        'versions': _versions,
-                                                        'app': _app,
-                                                        'os_type': _ver_type,
-                                                        'privilege':privilege,
-                                                        })
-    else:
-        return HttpResponseBadRequest()
+            'versions': _versions,
+            'app': _app,
+            'os_type': _ver_type,
+            'privilege': privilege,
+        })
+
+    @method_decorator(login_required)
+    def post(self, request):
+        _app_id = request.POST.get('app_id')
+        _os_type_f = request.POST.get('os_type')
+        _os_type_s = _os_type_f[0]  # Берем первую букву, она равна сокращениям
+        _ver_number = request.POST.get('ver_number')
+
+        app = Application.objects.get(id=_app_id)
+
+        # Апи не используется, нет смысла...
+        # _ver_changes = get_changes(app.source_code)
+        # api_con = ApiConnect()
+        # api_con.send_compile_request(
+        #     'uuid',
+        #     app.id,
+        #     _ver_number,
+        #     app.source_code,
+        #     _os_type_f,
+        # )
+
+        v = Version(
+            number=_ver_number,
+            application=app,
+            # path=api_con.file,
+            path='NotImplemented',
+            ver_type=_os_type_s,
+            status='NotImplemented',
+            # status = api_con.status,
+            # changes = _ver_changes,
+            changes='NotImplemented',
+        )
+
+        v.save()
+        # api_con.remove_file()
+
+        return HttpResponse('ok')
 
 
 @login_required
@@ -44,51 +80,6 @@ def add_version(request):
         _app = Application.objects.get(id=_app_id)
         _os_type = request.GET.get('os_type')
         return render(request, 'version/add.html', {'app': _app, 'os_type':_os_type})
-    else:
-        return HttpResponseBadRequest()
-
-
-@login_required
-def compile_ver(request):
-    if request.method == 'POST':
-        _app_id = request.POST.get('app_id')
-        _os_type_f = request.POST.get('os_type')
-        _os_type_s = _os_type_f[0] # Берем первую букву, она равна сокращениям
-        _ver_number = request.POST.get('ver_number')
-
-        app = Application.objects.get(id=_app_id)
-
-        _ver_changes = get_changes(app.source_code)
-
-        api_con = ApiConnect()
-        api_con.send_compile_request(
-            'uuid',
-            app.id,
-            _ver_number,
-            app.source_code,
-            _os_type_f,
-        )
-
-        v = Version(
-            number=_ver_number,
-            application=app,
-            path=api_con.file,
-            ver_type=_os_type_s,
-            status = api_con.status,
-            changes = _ver_changes,
-        )
-        v.save()
-        api_con.remove_file()
-
-        return render(
-            request,
-            'version/add_success.html',
-            {'app_id':app.id, 'ver_type':v.ver_type},
-        )
-    else:
-        return HttpResponseBadRequest()
-
-
 
 
 @login_required
